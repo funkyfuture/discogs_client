7decd30a39f22bce03cde3a7ee076c936b4b89b3
Frank Sachsenheim <funkyfuture@riseup.net>
2020-10-19 22:11:50 +0200

    Refactors code for currently supported Python 3.6 to 3.9

--- discogs_client/utils.py
+++ discogs_client/utils.py
@@ -1,14 +1,5 @@
-from __future__ import unicode_literals
-
 from datetime import datetime
-try:
-    # python2
-    from urllib2 import quote
-    to_str = unicode
-except ImportError:
-    # python3
-    from urllib.parse import quote
-    to_str = str
+from urllib.parse import quote
 
 
 def parse_timestamp(timestamp):
@@ -18,7 +9,7 @@ def parse_timestamp(timestamp):
 
 def update_qs(url, params):
     """A not-very-intelligent function to glom parameters onto a query string."""
-    joined_qs = '&'.join('='.join((str(k), quote(to_str(v).encode('utf8'))))
+    joined_qs = '&'.join('='.join((str(k), quote(str(v))))
                          for k, v in params.items())
     separator = '&' if '?' in url else '?'
     return url + separator + joined_qs
